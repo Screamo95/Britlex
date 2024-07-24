2d2703af493d9585024068cfa64478020431df91
Screamo95 <bykovglobal@gmail.com>
2024-07-24 16:48:42 +0300

    last commit

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,4 @@
+// Бургер меню
 document.addEventListener('DOMContentLoaded', function() {
   var sidenav = document.querySelectorAll('.sidenav');
   var instances_sidenav = M.Sidenav.init(sidenav);
